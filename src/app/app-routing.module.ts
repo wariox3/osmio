fdf73622c7bd80b4cda7bc28b16ef49581aa1789
sebastian <sebastian.h.piedrahita@gmail.com>
2025-04-09 16:37:39 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,107 +4,107 @@ import { AutentificacionGuard } from '@guardias/auth.guard';
 import { LandingpageComponent } from './pages/landingpage/landingpage.component';
 
 export const routes: Routes = [
-  // {
-  //   path: 'inicio',
-  //   component: LandingpageComponent,
-  // },
-  // {
-  //   path: 'faqs',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'terminos',
-  //   loadComponent: () =>
-  //     import('./pages/terminos/terminos.component').then(
-  //       (c) => c.TerminosComponent
-  //     ),
-  // },
-  // {
-  //   path: 'docs',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'cambios',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'tutoriales',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'ayuda',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'acerca_de',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'comprar',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'soporte',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'caracteristicas',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
-  // {
-  //   path: 'foro',
-  //   loadComponent: () =>
-  //     import('./pages/contruccion/contruccion.component').then(
-  //       (c) => c.ContruccionComponent
-  //     ),
-  // },
   {
-    path: 'auth',
-    loadChildren: () =>
-      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    path: 'inicio',
+    component: LandingpageComponent,
   },
   {
-    path: 'error',
-    loadChildren: () =>
-      import('./modules/errors/errors.module').then((m) => m.ErrorsModule),
+    path: 'faqs',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
   },
   {
-    path: '',
-    canActivate: [AutentificacionGuard],
-    loadChildren: () =>
-      import('./_metronic/layout/layout.module').then((m) => m.LayoutModule),
+    path: 'terminos',
+    loadComponent: () =>
+      import('./pages/terminos/terminos.component').then(
+        (c) => c.TerminosComponent
+      ),
   },
   {
-    path: '**',
-    redirectTo: 'auth',
+    path: 'docs',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
   },
+  {
+    path: 'cambios',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  {
+    path: 'tutoriales',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  {
+    path: 'ayuda',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  {
+    path: 'acerca_de',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  {
+    path: 'comprar',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  {
+    path: 'soporte',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  {
+    path: 'caracteristicas',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  {
+    path: 'foro',
+    loadComponent: () =>
+      import('./pages/contruccion/contruccion.component').then(
+        (c) => c.ContruccionComponent
+      ),
+  },
+  // {
+  //   path: 'auth',
+  //   loadChildren: () =>
+  //     import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  // },
+  // {
+  //   path: 'error',
+  //   loadChildren: () =>
+  //     import('./modules/errors/errors.module').then((m) => m.ErrorsModule),
+  // },
+  // {
+  //   path: '',
+  //   canActivate: [AutentificacionGuard],
+  //   loadChildren: () =>
+  //     import('./_metronic/layout/layout.module').then((m) => m.LayoutModule),
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: 'auth',
+  // },
 ];
 
 @NgModule({
